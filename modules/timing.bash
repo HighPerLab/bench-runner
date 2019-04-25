c2bd24856869912e2ac77c67f69e2b412f2e9c3a
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2019-04-25 17:59:16 +0100

    add function to get trace/throughput info
    This is only useful for NON-UVA cases (using memcpy). UVA is already
    handled by the existing nvprof profiling.

--- modules/timing.bash
+++ modules/timing.bash
@@ -21,7 +21,6 @@ timeit_cuda() {
     # params: binary runflags logfile stdin
     local -a runflags
     read -r -a runflags <<< "${2}"
-    local timerformat='elapsed: %e s'
     local stdin="${4:-/dev/null}"
     inf "Running with NVPROF"
     debug "RUN: ./${1} ${runflags[*]} < ${stdin} &> /dev/null"
@@ -29,4 +28,20 @@ timeit_cuda() {
     if ! nvprof -f --profile-api-trace all --track-memory-allocations on --unified-memory-profiling per-process-device -u ms --csv --log-file "${3}" ./"${1}" "${runflags[@]}" < "${stdin}" &> /dev/null; then
         error_handling
     fi
+}
+
+## with this function we can determine HtoD/DtoH bandwidths for the whole run.
+## The following UNIX tools sequence can be used to get the averages:
+##    grep HtoD example.csv.log | awk -F, '{ size+=$12; band+=$13; count+=1 } END { print size/count,band/count }'
+nvprof_trace() {
+    # params: binary runflags logfile stdin
+    local -a runflags
+    read -r -a runflags <<< "${2}"
+    local stdin="${4:-/dev/null}"
+    inf "Running with NVPROF GPU trace"
+    __log_noredirect 4 "INFO" "  CUDA trace output:" >> "${3}"
+    if ! nvprof -f --print-gpu-trace -u ms --csv --log-file "${3}" ./"${1}" "${runflags[@]}" < "${stdin}" &> /dev/null; then
+        error_handling
+    fi
+
 }
