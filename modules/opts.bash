09f80938aab1031039524acf5197c80378e79554
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2019-05-06 18:29:33 +0100

    added new stages option
    with this we can decide to only build, or only time, or trace... etc.

--- modules/opts.bash
+++ modules/opts.bash
@@ -2,28 +2,42 @@
 # Module provides function to parse commandline arguments
 #
 
-declare -A STAGES=( [build]=1 [run]=1 )
+declare -A STAGES=( [build]=true [profile]=true [time]=true [trace]=true )
 declare TARGET=true # we run all targets
 declare MOUNT=false
 declare VERBOSITY=0
 
 usage() {
     cat <<- HERE >&2
-    Usage: [-b] [-t <TARGET>] [-h]
-    -b           -- build only
-    -m           -- mount tmp dir (cluster specific)
-    -t <TARGET>  -- use only target '<TARGET>'
-    -v           -- verbosity (specify multiple times to increase)
-    -h           -- print this help message
+    Usage: [-m] [-v...] [-S <STAGE>,...] [-t <TARGET>] [-h]
+    -m              -- mount tmp dir (cluster specific)
+    -t <TARGET>     -- use only target '<TARGET>'
+    -v              -- verbosity (specify multiple times to increase)
+    -S <STAGE>,...  -- which stage to complete (build, profile, time, and trace)
+                       default: all
+    -h              -- print this help message and exit
 HERE
 }
 
 # param: arguments
 argparse() {
-    while getopts bhmt:v name; do
+    while getopts bhmt:S:v name; do
         case $name in
-            b)
-                STAGES[run]=0
+            S)
+                local stage
+                # zero out all stages
+                for stage in "${!STAGES[@]}"; do
+                    STAGES[$stage]=false
+                done
+                IFS=, read -ra stages <<< "${OPTARG}"
+                for stage in "${stages[@]}"; do
+                    if [[ ! "${!STAGES[*]}" =~ ${stage} ]]; then
+                        echo "Given stage \`${stage}' is not valid!" >&2
+                        exit 12
+                    else
+                        STAGES[$stage]=true
+                    fi
+                done
                 ;;
             m)
                 MOUNT=true
